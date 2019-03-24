3d2d0ff703710e5c82b0898f373d6f0d334fa0c7
John <john.herrerar@ciplima.org.pe>
2019-03-24 16:05:44 -0500

    Cuarta Version

--- tablas_ddl.sql
+++ tablas_ddl.sql
@@ -1,8 +1,10 @@
+--cambio en la rama alterna
 --creacion de tablas del data warehouse
 create table cliente(
 	nombre varchar(50),
 	dni char(8)
 )
 
+tablespace tmp
 --autor: John Herrera
 --tercer cambio
